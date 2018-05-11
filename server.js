b909988b51df54d2408fd7ba11badc6833350658
Glitch (lucie) <none>
2018-05-11 09:26:27 +0000

    💆🎄 Checkpoint ./server.js:1095253/1582

--- server.js
+++ server.js
@@ -69,23 +69,23 @@ db.serialize(function(){
   }
 });
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + '/views/index.html');
+// });
 
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
-});
+// // endpoint to get all the dreams in the database
+// // currently this is the only endpoint, ie. adding dreams won't update the database
+// // read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
+// app.get('/getDreams', function(request, response) {
+//   db.all('SELECT * from Dreams', function(err, rows) {
+//     response.send(JSON.stringify(rows));
+//   });
+// });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// // listen for requests :)
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
 
 
